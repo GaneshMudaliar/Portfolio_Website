aaefab148e4e22597119220ed9576f74b33e6601
GaneshMudaliar <mudaliarganesh858@gmail.com>
2025-02-18 16:53:36 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // Use Render's port or default to 3000 if not set
+  },
 })
